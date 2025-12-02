7bc446118240d1da26b4a5ae4e0136554ede8a84
Ruchitha Nandikonda <ruchithanandikonda@Ruchithas-Laptop-61.local>
2025-12-01 23:31:09 -0700

    Adjust Vanta.js background - smoother animation with better colors

--- JS/vanta-background.js
+++ JS/vanta-background.js
@@ -62,8 +62,8 @@ function initVantaBackground() {
         midtoneColor: colors.midtoneColor,
         lowlightColor: colors.lowlightColor,
         baseColor: colors.baseColor,
-        speed: 2.5,
-        zoom: 1.6,
+        speed: 1.0,
+        zoom: 0.8,
       });
       
       console.log("Vanta.js background initialized successfully", vantaEffect);
